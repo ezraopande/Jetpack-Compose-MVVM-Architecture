f897264f12c2905251bbafa31a2784ed2c40b193
Ezra <holla@apensoftwares.co.ke>
2024-08-17 11:39:38 +0300

    Added BottomBar & FAB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
 
 dependencies {
 
+    implementation (libs.androidx.material)
     implementation(libs.androidx.navigation.compose)
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/ezra/mvvmarchitecture/ui/theme/home/Home.kt
+++ app/src/main/java/com/ezra/mvvmarchitecture/ui/theme/home/Home.kt
@@ -1,21 +1,143 @@
 package com.ezra.mvvmarchitecture.ui.theme.home
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ezra.mvvmarchitecture.navigation.ROUTE_ABOUT
-import com.ezra.mvvmarchitecture.navigation.ROUTE_HOME
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Home(navController: NavController){
+fun Home(navController: NavController) {
+    var showFAB by remember { mutableStateOf(false) }
+    val selectedIndex = remember { mutableStateOf(0) }
 
-    Text(text = "Home Scree")
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Home") },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Home, contentDescription = "")
+                    }
+                },
+                modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars)
+            )
+        },
+        content = { paddingValues ->
+            Box(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .fillMaxSize()
 
-    Text(text = "Go to about",
-        modifier = Modifier
-            .clickable { navController.navigate(ROUTE_ABOUT) }
+                ) {
+                    Text(text = "Home Screen")
+
+                    Text(
+                        text = "Go to about",
+                        modifier = Modifier
+                            .clickable { navController.navigate(ROUTE_ABOUT) }
+                            .padding(top = 16.dp)
+                    )
+
+
+
+
+
+                }
+
+                if (showFAB) {
+                    Column(
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(bottom = 72.dp, end = 16.dp)
+                            .windowInsetsPadding(WindowInsets.systemBars),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        FloatingActionButton(onClick = { navController.navigate(ROUTE_ABOUT) }) {
+                            Icon(Icons.Default.Add, contentDescription = "Action 1")
+                        }
+                        FloatingActionButton(onClick = { /* TODO: Handle action 2 */ }) {
+                            Icon(Icons.Default.Edit, contentDescription = "Action 2")
+                        }
+                        FloatingActionButton(onClick = { /* TODO: Handle action 3 */ }) {
+                            Icon(Icons.Default.Delete, contentDescription = "Action 3")
+                        }
+                    }
+                }
+            }
+        },
+        bottomBar = {
+            BottomNavigation(
+                elevation = 10.dp,
+                modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars)
+            ) {
+
+                BottomNavigationItem(
+                    icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "") },
+                    label = { Text(text = "Home") },
+                    selected = (selectedIndex.value == 0),
+                    onClick = {
+                        showFAB = !showFAB
+                        selectedIndex.value = 0
+                    }
+                )
+
+                BottomNavigationItem(
+                    icon = { Icon(imageVector = Icons.Default.Favorite, contentDescription = "") },
+                    label = { Text(text = "Favorite") },
+                    selected = (selectedIndex.value == 1),
+                    onClick = {
+                        showFAB = false
+                        selectedIndex.value = 1
+                    }
+                )
+
+                BottomNavigationItem(
+                    icon = { Icon(imageVector = Icons.Default.Person, contentDescription = "") },
+                    label = { Text(text = "Profile") },
+                    selected = (selectedIndex.value == 2),
+                    onClick = {
+                        navController.navigate(ROUTE_ABOUT)
+                        showFAB = false
+                        selectedIndex.value = 2
+                    }
+                )
+            }
+        }
     )
-
 }
